b2f6d921bd00a2d1157ef6deca1201b2ed997f76
Stanislav Savciuc <stanislav@Stanislavs-MacBook-Pro.local>
2018-09-05 19:52:36 -0400

    (cleanup) Restructured the code into functional component

--- client/src/components/LogListItem.jsx
+++ client/src/components/LogListItem.jsx
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
 
-class LogListItem extends Component {
-	//passing the id of the item which was onClick function used on, to the LogList component so it would pass it up to the App component
-	deleteListItem(id) {
-		this.props.onDelete(id);
-	}
-	render() {
-		return (
-			//creating an <li> with classname ListItem so we could later use it for styling purposes
-			//inside we are receiving the item from the parent component which is LogList,
-			//adding to it a button with onClick function binded to the item ID and also to the delete function which is passed up to the parent.
-			<li className="ListItem">
-				{this.props.item}{' '}
-				<button className="listButton" onClick={this.deleteListItem.bind(this, this.props.item.id)}>
-					X
-				</button>
-			</li>
-		);
-	}
-}
+const LogListItem = (props) => {
+	//creating an <li> with classname ListItem so we could later use it for styling purposes
+	//inside we are receiving the item from the parent component which is LogList,
+	//adding to it a button with onClick function binded to the item ID and also to the delete function which is passed up to the parent.
+	return (
+		<li className="ListItem">
+			{this.props.item}{' '}
+			<button className="listButton" onClick={this.props.onDelete(this.props.item.id)}>
+				X
+			</button>
+		</li>
+	);
+};
 
 export default LogListItem;
