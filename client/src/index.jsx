25d8189e684b2aab45350fc8d172dfa3daacce27
Stanislav Savciuc <stanislav@Stanislavs-MacBook-Pro.local>
2018-09-05 11:41:21 -0400

    Added app component

---
+++ client/src/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return;
+	}
+}
+ReactDOM.render(<App />, document.getElementById('app'));
