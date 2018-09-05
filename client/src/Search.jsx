3865d93d4fc67cf4fe5ee5c13907ab96459eb8fe
Stanislav Savciuc <stanislav@Stanislavs-MacBook-Pro.local>
2018-09-05 17:27:09 -0400

    Added the Search Component with dynamic variables which depends on which component we are at the moment

---
+++ client/src/Search.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			searchValue: ''
+		};
+		//setting the searchValue to the whatever was the input, also we need this as a separate function to prevent searching without actually clicking search
+		handleOnChange = (event) => {
+			//preventing the default behavior of the component, without this the page would refresh after every change
+			event.preventDefault();
+			//setting the searchValue to the value of the what text we have in the input field
+			this.setState({ searchValue: event.target.value });
+		};
+		//on click on the Search Button, we are passing the Search value of this state to the parent component Search function which would handle
+		// the api request
+		handleSubmit = (event) => {
+			//preventing the refreshing of the page, just showing the results
+			event.preventDefault();
+			this.props.onSearch(event, this.state.searchValue);
+		};
+	}
+	render() {
+		//search text means the text we want to have in our phrase before the input field, which would give a hint to user for what to search
+		// depends on the either we are in the food component or exercise
+		var searchText = this.props.showFood === true ? 'food' : 'exercise';
+		var placeholder = `describe your ${searchText}`;
+		//creating below a div element with a classname for further styling
+		//inside creating a label which would describe to the user what he can search for in this component
+		//creating a button which would handle the sending of the searched value to the parent component
+		return (
+			<div className="SearchBar">
+				<label className="SearchText">Write what type of {searchText} you would like to look for: </label>
+				<input type="text" value={value} onChange={this.handleOnChange.bind(this)} placeholder={placeholder} />
+				<button className="" type="submit" onSubmit={this.handleSubmit.bind(this)}>
+					Look it Up!
+				</button>
+			</div>
+		);
+	}
+}
+
+export default Search;
