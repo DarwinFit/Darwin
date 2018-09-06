a9d65feec4865cfceaeee6e0085511562a18285f
myj899 <myj899@gmail.com>
2018-09-06 10:47:55 -0400

    (feat) added chartist into package.json

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,7 +5,23 @@ import axios from 'axios';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			food_nutrition: {
+				sugar: 0,
+				calorie: 0,
+				carbs: 0,
+				protein: 0,
+				fat: 0
+			},
+			daily_nutrition: {
+				sugar: 0,
+				calorie: 0,
+				carbs: 0,
+				protein: 0,
+				fat: 0
+			},
+			foodLog: []
+		};
 	}
 	render() {
 		return;
