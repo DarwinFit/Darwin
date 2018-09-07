258ec7f00259f2768407ab024eb4075a2bd0e061
Stanislav Savciuc <stanislav@Stanislavs-MacBook-Pro.local>
2018-09-07 11:13:17 -0400

    (fix) Fixed the search/food/exercise component to work with dynamic type of values

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -13,6 +13,7 @@ class Search extends Component {
 	handleOnChange(event) {
 		//preventing the default behavior of the component, without this the page would refresh after every change
 		event.preventDefault();
+		console.log('DATA RECEIVED', event.target.value);
 		//setting the searchValue to the value of the what text we have in the input field
 		this.setState({ searchValue: event.target.value });
 	}
@@ -21,13 +22,14 @@ class Search extends Component {
 	handleSubmit(event) {
 		//preventing the refreshing of the page, just showing the results
 		event.preventDefault();
+		console.log('SEARCHE VALUE FROM THE STATE', this.state.searchValue);
 		this.props.onSearch(this.state.searchValue);
 	}
 
 	render() {
 		//search text means the text we want to have in our phrase before the input field, which would give a hint to user for what to search
 		// depends on the either we are in the food component or exercise
-		var searchText = this.props.showFood === true ? 'food' : 'exercise';
+		var searchText = this.props.type ? 'food' : 'exercise';
 		var placeholder = `describe your ${searchText}`;
 		//creating below a div element with a classname for further styling
 		//inside creating a label which would describe to the user what he can search for in this component
@@ -41,7 +43,7 @@ class Search extends Component {
 					onChange={this.handleOnChange}
 					placeholder={placeholder}
 				/>
-				<button className="" type="submit" onSubmit={this.handleSubmit}>
+				<button className="" type="submit" onClick={this.handleSubmit}>
 					Look it Up!
 				</button>
 			</div>
