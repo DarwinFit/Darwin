a6f248511547aa3b316530c87ccd4bf40a8056f4
Stanislav Savciuc <stanislav@Stanislavs-MacBook-Pro.local>
2018-09-06 20:04:07 -0400

    (fix) Rendered the exercise component to the page

--- client/src/components/ExerciseDetails.jsx
+++ client/src/components/ExerciseDetails.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
-const ExerciseDetail = ({ exerciseData }) => {
+const ExerciseDetail = ({ exerciseData, username, handleAddExercise }) => {
 	return (
 		//creating a div with className for future styling
 		//Inside of it, we displaying the username which was passed in by props, with the exercise results for searched exercise
 		//Adding a button to handle adding it to the list of the exercises by executing the function from the main component
 		<div className="exerciseDetail">
-			<h2>Hey ,{this.props.username}, bellow are the details of your searched exercise results!</h2>
+			<h2>Hey ,{username}, bellow are the details of your searched exercise results!</h2>
 			<p>
 				During your exercise you will spend {exerciseData.duration_min} minutes {exerciseData.name} and you will
 				burn {exerciseData.nf_calories} !
 			</p>
 			<br />
-			<button className="AddButton" onClick={this.props.handleAddExercise}>
+			<button className="AddButton" onClick={handleAddExercise}>
 				Add it to your Done list for today!
 			</button>
 		</div>
