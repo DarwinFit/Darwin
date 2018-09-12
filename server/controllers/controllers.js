58927937df3168605ddabb4af9e61be9787e9dfd
Sarah Gujadhur <sarahgujadhur@gmail.com>
2018-09-12 14:07:08 -0400

    worked on searchExerciseEntry and createExerciseEntry

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -15,7 +15,7 @@ module.exports = {
   //retrieving user's information from database - WORKS
   getUserInfo: (req, res) => {
     // console.log('This is request in get:', req.query.username);
-    var { username } = req.query;
+    let { username } = req.query;
     models.getUserInfo.get(username, (err, results) => {
       if (err) console.log('Error caught on getUserInfo in controller.js', err);
       else res.send(results);
@@ -72,8 +72,8 @@ module.exports = {
                 let oldNutrient = nutrientsToBeUpdated[key];
                 nutrientsToBeUpdated[key] = oldNutrient + data.foods[0][`nf_${key}`];
               }
-              models.updateDaily.post(nutrientsToBeUpdated, query,(err, entry) => {
-                if (err) console.log('Error caught at updateDaily in controllers', err);
+              models.updateDailyForFood.post(nutrientsToBeUpdated, query, (err, entry) => {
+                if (err) console.log('Error caught at updateDailyForFood in controllers', err);
                 console.log('Database has been updated with new food entry');
                 res.sendStatus(201);
               })
@@ -92,18 +92,34 @@ module.exports = {
     });
   },
 
-  //API request - WORKS
-  searchExerciseEntry: (req, res) => {
-    let query = {exercise_name: req.body.exercise_name,
-                  gender: req.body.gender, 
-                  weight_kg: req.body.weight_kg, 
-                  height_cm: req.body.height_cm,
-                  age: req.body.age
-                };
-    getExercises(query, (err, data) => {
-      if (err) console.log('Error inside searchExerciseEntry in controllers.js', err); 
-      else res.send(data.exercises);
-    });
+  //API request - WORKS - FIX SO THAT USER DOESN'T NEED TO INPUT ALL
+  searchExerciseEntry: (req, res) => { //req === exercise_name, user_id, date, usernmae
+    // console.log('first:', req.body)
+    async function userInfo() {
+      models.getUserInfo.get(req.body.username, (err, result) => {
+        if (err) console.log('Error caught at userInfo in searchExerciseEntry in controllers.js', err);
+        console.log('result', result);
+        let user = result;
+        return user;
+      });
+    };
+
+    async function getExercise () {
+      let userDetails = await userInfo();
+      console.log('userDEETS:', userDetails); 
+      let query = {query: req.body.exercise_name,
+                    gender: userDetails.gender, 
+                    weight_kg: userDetails.weight, 
+                    height_cm: userDetails.height,
+                    age: userDetails.age
+                  };
+        console.log("query is ", query);
+      getExercises(query, (err, data) => {
+        if (err) console.log('Error inside searchExerciseEntry in controllers.js', err); 
+        else res.send(data.exercises);
+      });
+    }
+    getExercise();
   },
 
   //when 'add exercise' is clicked
@@ -112,6 +128,7 @@ module.exports = {
     let {user_id, date, exercise_name} = req.body;
     let query = [user_id, date];
     let body = [exercise_name, user_id, date];
+    let burntToBeUpdated = {};
     //check daily burnt of user for first entry of the day or just update entry
     models.getDaily.get(query, (err, result) => {
       // console.log('result from getDaily is', result);
@@ -133,15 +150,30 @@ module.exports = {
       }
       //updating user's burnt intake
       else {
-        // console.log('user already has a daily row, will update burnt');
-        models.saveAndUpdateExerciseEntry.post(body, (err, result) => {
-          if (err) console.log('Error inside createExerciseEntry in controllers.js at 4th callback', err);
-          else {
-            models.getExerciseEntry.get(query, (err, result) => {
-              if (err) console.log('Error inside createExerciseEntry in controllers.js at 5th callback', err);
-              else res.send(result);
+        models.insertIntoExerciseHistory.post(body, (err, result) => {
+          if (err) console.log('Error caught at insertIntoExerciseHistory in controllers', err);
+          models.getDailyForExercise.get(query, (err, results) => {
+            if (err) console.log('Error caught at getDailyForExercise in controllers', err);
+            burntToBeUpdated = results[0];
+
+            let exerciseQuery = {exercise_name: exercise_name,
+              gender: req.body.gender, 
+              weight_kg: req.body.weight_kg, 
+              height_cm: req.body.height_cm,
+              age: req.body.age
+            };
+
+            getExercises(exerciseQuery, (err, data) => { //exercise_name needs to filled up properly
+              if (err) console.log('Error inside getExercise in createExerciseEntry in controllers.js', err);
+              let oldNutrient = burntToBeUpdated[burnt];
+              burntToBeUpdated[burnt] = oldNutrient + data.exercise[0]['nf_calories'];
+              models.updateDailyForExercise.post(burntToBeUpdated, query, (err, entry) => {
+                if (err) console.log('Error caught at updateDailyForExercise in controllers', err);
+                console.log('Database has been updated with new exercise entry');
+                res.sendStatus(201);
+              })
             })
-          }
+          })
         })
       }
     })
@@ -155,7 +187,9 @@ module.exports = {
   },
 
   getDaily: (req, res) => { //WORKS
-    models.getDaily.get(req, (err, result) => {
+    let { user_id, date  } = req.query;
+    let query = [user_id, date];
+    models.getDaily.get(query, (err, result) => {
       if (err) console.log('Error caught on models.getDaily in controllers', err);
       else res.send(result);
     })
