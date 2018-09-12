97bd4aa3e6f3fbad35db371f99e838e5b941708d
Stanislav Savciuc <stanislav@Stanislavs-MacBook-Pro.local>
2018-09-12 15:25:40 -0400

    (dev)(cleanup)(feat) Added Logout/PopUp components, Cleaned up the code from not needed elements, refactored the NavBar to be working with Popups

--- client/src/components/NavHome.jsx
+++ client/src/components/NavHome.jsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 //importing destructured NavLink from react
 import { NavLink } from 'react-router-dom';
+import Popup from './PopUp.jsx';
 
-const NavHome = () => {
+const NavHome = (props) => {
 	return (
 		//creating a NavBar which will redirect to any component user would click on
 		//adding className to the div for future styling, could also ad activeClassName for each navlink separate, matter of choice
-
+		//importing PopUp component, and using instead of NavLinks, since to active it we need to click on it
 		<div className="Navbar">
-			<NavLink to='/' activeClassName="NavBarLogo">HealthApp</NavLink>
-			<NavLink to='/login'>Login</NavLink>
-			<NavLink to='/signup'>Signup</NavLink>
+			<NavLink to="/" activeClassName="NavBarLogo">
+				HealthApp
+			</NavLink>
+			<Popup uiConfig={props.uiConfig} page={'Login'} />
+			<Popup uiConfig={props.uiConfig} page={'Sign Up'} />
 		</div>
 	);
 };
