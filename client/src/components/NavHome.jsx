c625f1774a5c9d682c0c433d2e1acb39c5562c0e
myj899 <myj899@gmail.com>
2018-09-13 13:02:19 -0400

    (feat) finished styling for main page

--- client/src/components/NavHome.jsx
+++ client/src/components/NavHome.jsx
@@ -8,16 +8,29 @@ const NavHome = (props) => {
 	return (
 		//creating a NavBar which will redirect to any component user would click on
 		//adding className to the div for future styling, could also ad activeClassName for each navlink separate, matter of choice
-		<ul className='nav'>
-			<li className='nav-brand'><NavLink to='/'>HealthApp</NavLink></li>
-			<li>
-				<button className='nav-login'>
-					<Popup uiConfig={props.uiConfig} page={'Login'}/>
+		// <ul className='nav'>
+		// 	<li className='nav-brand'><NavLink to='/'>HealthApp</NavLink></li>
+		// 	<li>
+		// 		<button className='nav-login'>
+		// 			<Popup uiConfig={props.uiConfig} page={'Login'}/>
+		// 		</button>
+		// 	</li>
+		// 	<li>
+		// 		<button className='nav-signup'>
+		// 			<Popup uiConfig={props.uiConfig} page={'Sign Up'}/>
+		// 		</button>
+		// 	</li>
+		// </ul>
+		<ul className='navhome'>
+			<li className='navhome-brand'><NavLink to='/'>HealthApp</NavLink></li>
+			<li className='navhome-button'>
+				<button className='navhome-signup'>
+					<Popup uiConfig={props.uiConfig} page={'SIGN UP'}/>
 				</button>
 			</li>
-			<li>
-				<button className='nav-signup'>
-					<Popup uiConfig={props.uiConfig} page={'Sign Up'}/>
+			<li className='navhome-button'>
+				<button className='navhome-login'>
+					<Popup uiConfig={props.uiConfig} page={'LOGIN'}/>
 				</button>
 			</li>
 		</ul>
