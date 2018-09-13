a7f6510fd0a3e9ce513b0fb5b3a3bdc4325fae36
myj899 <myj899@gmail.com>
2018-09-13 07:21:41 -0400

    (cleanup) refactoring with Firebase code from Stas done

--- client/src/components/CalorieGraph.jsx
+++ client/src/components/CalorieGraph.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-var LineChart = require('react-chartjs').Line;
-const Chart = require('react-chartjs');
+// var LineChart = require('react-chartjs').Line;
+// const Chart = require('react-chartjs');
 
-Chart.responsive = true;
-/* Add a basic data series with six labels and values */
-let chartData = {
-  xAxisID: 'Date',
-  yAxisID: 'Calories [kcal]',
-  borderColor: 'rgba(0, 0, 0, 0.1)',
-  borderWidth: 3,
-  data: [{x: 0, y: 1780}, {x: 1, y: 1800}, {x: 2, y: 1900}, {x: 3,y: 1900}, 
-         {x: 4, y: 1900}, {x: 5, y: 1900}, {x: 6,y: 1900}, {x: 7, y: 1900}]
-};
+// Chart.responsive = true;
+// /* Add a basic data series with six labels and values */
+// let chartData = {
+//   xAxisID: 'Date',
+//   yAxisID: 'Calories [kcal]',
+//   borderColor: 'rgba(0, 0, 0, 0.1)',
+//   borderWidth: 3,
+//   data: [{x: 0, y: 1780}, {x: 1, y: 1800}, {x: 2, y: 1900}, {x: 3,y: 1900}, 
+//          {x: 4, y: 1900}, {x: 5, y: 1900}, {x: 6,y: 1900}, {x: 7, y: 1900}]
+// };
 
-let chartOptions = {
-  showLines: true,
-  spanGaps: true
-};
+// let chartOptions = {
+//   showLines: true,
+//   spanGaps: true
+// };
 
 const CalorieGraph = () => (
   <div>
