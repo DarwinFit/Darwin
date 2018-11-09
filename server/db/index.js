75ef94f98e536e6a0abaac600bf0e4b20f385b56
myj899 <myj899@gmail.com>
2018-11-09 15:40:50 -0500

    config require fixed

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const config = {
-	host: 'russel.cfjtr9fufcj4.us-east-1.rds.amazonaws.com',
-	user: 'teamRussell',
-	password: 'teamRuSSell',
-	database: 'russell'
+  host: 'russel.cfjtr9fufcj4.us-east-1.rds.amazonaws.com',
+  user: 'teamRussell',
+  password: 'teamRuSSell',
+  database: 'russell'
 };
 
 const db = mysql.createConnection(config);
