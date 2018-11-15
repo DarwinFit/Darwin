993871ba0a1d7b8634fee37bf78995a99997b144
myj899 <myj899@gmail.com>
2018-11-14 19:45:34 -0500

    DB name change for schema

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 
-CREATE DATABASE IF NOT EXISTS teamRussell;
+CREATE DATABASE IF NOT EXISTS darwinFit;
 
-USE teamRussell;
+USE darwinFit;
 
 CREATE TABLE if not exists users (
   id int NOT NULL AUTO_INCREMENT,
